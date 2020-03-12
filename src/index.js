cd061862c3010db64827926a6f15ead1de67f716
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-03-12 19:42:51 +0300

    also Easy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined) {
+		return 0;}
+	if  (array.length==0){
+		console.log('here')
+		return 0;}
+	var k=array[0];
+	for(var i=0;i<array.length;i++){
+		if(k>array[i]){
+			k=array[i];
+		}
+	}
+	return k
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined) {
+		return 0;}
+	if  (array.length==0){
+		console.log('here')
+		return 0;}
+	var k=array[0];
+	for(var i=0;i<array.length;i++){
+		if(k<array[i]){
+			k=array[i];
+		}
+	}
+	return k
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array===undefined) {
+		return 0;}
+	if  (array.length==0){
+		console.log('here')
+		return 0;}
+	var k=0;
+	for(var i=0;i<array.length;i++){
+		
+			k += array[i];
+		
+	}
+	return k/array.length;
 }
